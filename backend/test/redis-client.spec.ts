7f4821cffde326f44c7bfc541fc14c928e048302
Artem Belyavsky <artishake.dev@gmail.com>
2023-10-29 23:59:43 +0300

    s1-7/build: add redis
    Add redis client integration.
    Add redis docker service.

---
+++ backend/test/redis-client.spec.ts
@@ -0,0 +1,16 @@
+import { createClient } from 'redis';
+
+describe('Test redis connectors', () => {
+    test('Test redis connector: write and read value', async () => {
+        const client = await createClient()
+            .on('error', err => console.log('Redis Client Error', err))
+            .connect();
+
+        await client.set('nimbus_key', 'nimbus_value');
+        const value = await client.get('nimbus_key');
+
+        expect(value).toEqual('nimbus_value');
+
+        await client.disconnect();
+    });
+});
