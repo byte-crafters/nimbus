9182ff6542611239f5634be767de6863f7384d78
Artem Belyavsky <artishake.dev@gmail.com>
2023-11-03 22:59:19 +0300

    s1-8/feat: Add test for mocked method

--- backend/src/modules/users/users.service.spec.ts
+++ backend/src/modules/users/users.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { UsersService, mockUsersCollection } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+    let usersService: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [UsersService],
+        }).compile();
 
-    service = module.get<UsersService>(UsersService);
-  });
+        usersService = module.get<UsersService>(UsersService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(usersService).toBeDefined();
+    });
+
+    it('should return existing user by username', async () => {
+        const targetUsername = 'one';
+        const searchResult = await usersService.findOne(targetUsername);
+
+        expect(searchResult.username).toEqual(targetUsername);
+    });
+
+    it('should NOT return user by username', async () => {
+        const targetUsername = 'ffqf4f';
+        const searchResult = await usersService.findOne(targetUsername);
+
+        expect(searchResult).toBeUndefined();
+    });
+
+    it('should return all users', async () => {
+        jest.spyOn(usersService, 'findAll').mockImplementationOnce(async () => mockUsersCollection);
+        const searchResult = await usersService.findAll();
+
+        expect(searchResult).toEqual(expect.arrayContaining(mockUsersCollection));
+    });
 });
--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 import { User } from './User';
+
+export const mockUsersCollection = [
+    new User({ password: '123', username: 'one' }),
+    new User({ password: 'qwe', username: 'two' })
+];
 
 @Injectable()
 export class UsersService {
-    async findOne(username: string): Promise<User | null> {
-        return new User({ password: 'pass', username: 'artishake' });
+    async findOne(username: string): Promise<User | undefined> {
+        return mockUsersCollection.find((user) => user.username === username);
+    }
+
+    async findAll(): Promise<User[] | null> {
+        throw new NotImplementedException();
     }
 }
