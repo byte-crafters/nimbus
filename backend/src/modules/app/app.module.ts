9b326e48651e0e60e5a751fce258ca0e923e46d4
Artem Belyavsky <artishake.dev@gmail.com>
2023-11-03 19:07:04 +0300

    s1-8/feat: Fix auth module tests

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { AppServiceModule } from '../app-service/app-service.module';
-import { jwtConstants } from '@modules/auth/constants';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
     imports: [
         AuthModule,
         AppServiceModule,
-        JwtModule.register({
-            global: true,
-            secret: jwtConstants.secret,
-            signOptions: { expiresIn: '60s' },
-        })
     ],
 })
 export class AppModule { }
--- backend/src/modules/auth/auth.controller.spec.ts
+++ backend/src/modules/auth/auth.controller.spec.ts
@@ -1,11 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { JwtService, JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { jwtConstants } from './constants';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
     let controller: AuthController;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                UsersModule,
+                JwtModule.register({
+                    global: true,
+                    secret: jwtConstants.secret,
+                    signOptions: { expiresIn: '60s' },
+                })
+            ],
+            providers: [AuthService],
             controllers: [AuthController],
         }).compile();
 
