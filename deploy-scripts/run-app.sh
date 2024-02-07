c8804892d0130d59eaa675444049f24761d05614
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-07 19:56:03 +0300

    build: deploy

--- deploy-scripts/run-app.sh
+++ deploy-scripts/run-app.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# pwd
-echo "--1"
-ls $pwd
-echo "--2"
-ls /var/nimbus-app/
-echo "--3"
 cd /var/nimbus-app/
-# cd ./backend
-docker-compose --file docker-compose.deploy.dev start caddy_reverse_proxy
+docker-compose --file docker-compose.deploy.dev.yaml start caddy_reverse_proxy
