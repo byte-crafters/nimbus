cc4424ec2b29122b8c26b9a8ece10837337f02db
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-07 20:21:40 +0300

    build: deploy

--- deploy-scripts/run-app.sh
+++ deploy-scripts/run-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/nimbus-app/
-docker-compose --file docker-compose.deploy.dev.yaml up
+docker-compose --file docker-compose.deploy.dev.yaml down -v && docker-compose --file docker-compose.deploy.dev.yaml up -d
