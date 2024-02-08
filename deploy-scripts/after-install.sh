d25597043348fde520628067468070d51a16ae0d
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-08 04:55:08 +0300

    build: deploy

--- deploy-scripts/after-install.sh
+++ deploy-scripts/after-install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+echo "start"
 cd /var/nimbus-app/
+echo "1. load env vars "
 ./secondary/load-envs.sh
+echo "2. start docker containers"
 \
     docker-compose --file docker-compose.deploy.staging.yaml down -v \
     && docker-compose --file docker-compose.deploy.staging.yaml up -d
 cd backend
+echo "3. run npm install"
+node --version
+npm --version
 npm install
 npm run start
