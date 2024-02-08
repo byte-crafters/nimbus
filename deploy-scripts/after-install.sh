2f99fd5160977b83d823fe10164244f61a59a25b
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-08 06:37:18 +0300

    build: deploy

--- deploy-scripts/after-install.sh
+++ deploy-scripts/after-install.sh
@@ -8,7 +8,12 @@ echo "2. start docker containers"
     docker-compose --file docker-compose.deploy.staging.yaml down -v \
     && docker-compose --file docker-compose.deploy.staging.yaml up -d
 echo $USER
-./before-install.sh
+# ./before-install.sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+
 cd backend
 echo "3. run npm install"
 node --version
