99ca29171ad9ce9c7b2dda35bdbfbde4eeedd484
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-08 04:35:32 +0300

    build: deploy

--- deploy-scripts/after-install.sh
+++ deploy-scripts/after-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/nimbus-app/
-
+./secondary/load-envs.sh
 \
     docker-compose --file docker-compose.deploy.staging.yaml down -v \
     && docker-compose --file docker-compose.deploy.staging.yaml up -d
