0a91241bb04b2c8acce6e13f24ddc78b9b43b18b
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-08 06:33:13 +0300

    build: deploy

--- deploy-scripts/after-install.sh
+++ deploy-scripts/after-install.sh
@@ -7,9 +7,9 @@ echo "2. start docker containers"
 \
     docker-compose --file docker-compose.deploy.staging.yaml down -v \
     && docker-compose --file docker-compose.deploy.staging.yaml up -d
-cd backend
 echo $USER
 ./before-install.sh
+cd backend
 echo "3. run npm install"
 node --version
 npm --version
