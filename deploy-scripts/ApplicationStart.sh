696921602820e33bb27fb3d3c5b4335a625383bb
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-14 20:21:59 +0300

    build: pulling

--- deploy-scripts/ApplicationStart.sh
+++ deploy-scripts/ApplicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/nimbus-app/
 source ./deploy-scripts/secondary/load-envs.sh
-docker-compose --file docker-compose.deploy.staging.yaml up -d
+docker-compose --file docker-compose.deploy.staging.yaml up -d --pull
 exit 0
