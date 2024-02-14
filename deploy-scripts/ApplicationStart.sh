008b933e0d7316f08359c6d5a6eb798d6f752433
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-15 00:45:48 +0300

    build: deploy

--- deploy-scripts/ApplicationStart.sh
+++ deploy-scripts/ApplicationStart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/nimbus-app/
 source ./deploy-scripts/secondary/load-envs.sh
-docker-compose --file docker-compose.deploy.staging.yaml up -d --pull
+docker pull 913580947034.dkr.ecr.eu-north-1.amazonaws.com/nimbus-image-registry:nimbus_api-latest
+docker-compose --file docker-compose.deploy.staging.yaml up -d
 exit 0
