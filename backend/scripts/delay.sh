360c9101266124aea89f9dc2f6336b40f961f213
Artem Belyavsky <artishake.dev@gmail.com>
2024-02-16 18:37:06 +0300

    build: delay script fix

--- backend/scripts/delay.sh
+++ backend/scripts/delay.sh
@@ -1,14 +1,18 @@
-TRY_COUNT=0
-TRY_CONNECT_CODE=-1
+# TRY_COUNT=0
+# TRY_CONNECT_CODE=-1
 
-echo Connect code=$TRY_CONNECT_CODE
-until [ $TRY_CONNECT_CODE -eq 0 ]
-do 
-    mongosh -u root -p root --eval "rs.initiate();"
-    TRY_CONNECT_CODE=$?
-    ((TRY_COUNT=TRY_COUNT+1))
-    echo Connect try=$TRY_COUNT... code=$TRY_CONNECT_CODE
-    sleep 1
-done
+# echo Connect code=$TRY_CONNECT_CODE
+# until [ $TRY_CONNECT_CODE -eq 0 ]
+# do 
+#     mongosh -u root -p root --eval "rs.initiate();"
+#     TRY_CONNECT_CODE=$?
+#     ((TRY_COUNT=TRY_COUNT+1))
+#     echo Connect try=$TRY_COUNT... code=$TRY_CONNECT_CODE
+#     sleep 1
+# done
 
-echo "---DONE"
+# echo "---DONE"
+
+
+sleep 10s
+mongosh -u root -p root --eval "rs.initiate();"
