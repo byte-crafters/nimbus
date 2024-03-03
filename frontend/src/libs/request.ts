cea700d9d302b108e0ac4674fcdaf75c96a0322d
Maksim Beliavsky <maksimbell503@gmail.com>
2024-03-03 15:33:51 +0300

    add prettier config v2

--- frontend/src/libs/request.ts
+++ frontend/src/libs/request.ts
@@ -1,49 +1,49 @@
 export const METHODS = {
     GET: 'GET',
     POST: 'POST',
-}
+};
 
 export const HEADER = {
     Accept: 'Accept',
     ContentType: 'Content-Type',
-}
+};
 
 export const HEADERS_VALUE = {
     JSON: 'application/json',
-}
+};
 
 export type TFolder = {
-    parentId: string
-    name: string
-    id: string
-    owner: string
-}
+    parentId: string;
+    name: string;
+    id: string;
+    owner: string;
+};
 
 /**
  * Dont bind to method because it changes!
  */
 export type TGetChildrenFolders = {
-    parentFolder: TFolder
-    folders: TFolder[]
-}
+    parentFolder: TFolder;
+    folders: TFolder[];
+};
 
 export type TPostCreateFolder = {
-    parentFolder: TFolder
-    folders: TFolder[]
-}
+    parentFolder: TFolder;
+    folders: TFolder[];
+};
 
 export type TGetUserRootFolderChildren = {
-    parentFolder: TFolder
-    folders: TFolder[]
-}
+    parentFolder: TFolder;
+    folders: TFolder[];
+};
 
 export type TGetUserProfile = {
-    rootFolder: TFolder
-    id: string
-}
+    rootFolder: TFolder;
+    id: string;
+};
 
 export class Requester {
-    private host: string = process.env.NEXT_PUBLIC_NIMBUS_API_HOST ?? ''
+    private host: string = process.env.NEXT_PUBLIC_NIMBUS_API_HOST ?? '';
 
     register(login: string, password: string) {
         return fetch(`${this.host}/api/v1/auth/register`, {
@@ -57,7 +57,7 @@ export class Requester {
                 [HEADER.Accept]: HEADERS_VALUE.JSON,
                 [HEADER.ContentType]: HEADERS_VALUE.JSON,
             },
-        }).then(this.handleResponse)
+        }).then(this.handleResponse);
     }
 
     getUserProfile(): Promise<TGetUserProfile> {
@@ -68,7 +68,7 @@ export class Requester {
                 [HEADER.Accept]: HEADERS_VALUE.JSON,
                 [HEADER.ContentType]: HEADERS_VALUE.JSON,
             },
-        }).then(this.handleResponse)
+        }).then(this.handleResponse);
     }
 
     getUserRootFolder(): Promise<TGetUserRootFolderChildren> {
@@ -79,7 +79,7 @@ export class Requester {
                 [HEADER.Accept]: HEADERS_VALUE.JSON,
                 [HEADER.ContentType]: HEADERS_VALUE.JSON,
             },
-        }).then(this.handleResponse)
+        }).then(this.handleResponse);
     }
 
     getChildrenFolders(folderId: string): Promise<TGetChildrenFolders> {
@@ -90,7 +90,7 @@ export class Requester {
                 [HEADER.Accept]: HEADERS_VALUE.JSON,
                 [HEADER.ContentType]: HEADERS_VALUE.JSON,
             },
-        }).then(this.handleResponse)
+        }).then(this.handleResponse);
     }
 
     postCreateFolder(
@@ -108,19 +108,19 @@ export class Requester {
                 [HEADER.Accept]: HEADERS_VALUE.JSON,
                 [HEADER.ContentType]: HEADERS_VALUE.JSON,
             },
-        }).then(this.handleResponse)
+        }).then(this.handleResponse);
     }
 
     private handleResponse = (response: Response) => {
         if (response.ok) {
-            return response.json()
+            return response.json();
         } else {
             if (response.status === 502) {
-                console.error(502, response.statusText)
+                console.error(502, response.statusText);
             }
-            throw new Error()
+            throw new Error();
         }
-    }
+    };
 }
 
-export const fetcher = new Requester()
+export const fetcher = new Requester();
