0f8afa84f0305f88553ad8ddbdcafbc4c045f4c1
Artem Belyavsky <artembell.dev@gmail.com>
2024-04-07 20:41:54 +0300

    s4-2/build: add universal storage path resolving for any machine

--- backend/src/modules/files/constants.ts
+++ backend/src/modules/files/constants.ts
@@ -1,5 +1,6 @@
 import path from "node:path";
 
 export const FILES = {
-    FILES_PATH: path.join(path.resolve(process.cwd()), "_files")
+    FILES_PATH: path.join(path.resolve(process.cwd()), "_files"),
+    TEST_FILES_PATH: path.join(path.resolve(process.cwd()), "_test_files")
 };
