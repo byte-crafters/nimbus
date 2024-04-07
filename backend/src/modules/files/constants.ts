3f39b044653b1fa2fb9174af91e6998287336b01
Artem Belyavsky <artembell.dev@gmail.com>
2024-04-07 11:32:19 +0300

    s4-5/feat: add files folder automatic path config

--- backend/src/modules/files/constants.ts
+++ backend/src/modules/files/constants.ts
@@ -1,6 +1,5 @@
+import path from "node:path";
+
 export const FILES = {
-    // FILES_PATH: '/home/dwarfie/nimbus-app-sandbox/nimbus-files-test'
-    FILES_PATH: '/home/dwarfie/projects/nimbus/_files',
-    // FILES_PATH: "D:/Artem/Projects/nimbus/backend/@temp-files",
-    // TEST_FILES_PATH: "D:/Artem/Projects/nimbus/backend/@temp-files"
+    FILES_PATH: path.join(path.resolve(process.cwd()), "_files")
 };
