15143991004d64a8bc35a62547fe2327c7b0fd7e
Artem Belyavsky <artembell.dev@gmail.com>
2024-05-29 00:29:28 +0300

    s5-4/feat: add file dnd and uploading options

--- frontend/src/shared/Sidebar/Sidebar.tsx
+++ frontend/src/shared/Sidebar/Sidebar.tsx
@@ -2,10 +2,14 @@
 import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
 import UploadIcon from '@mui/icons-material/Upload';
 import { Button, Stack, Toolbar } from '@mui/material';
-import { PropsWithChildren, useRef } from 'react';
+import { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import styles from './Sidebar.module.scss';
+import stylesDropzone from './styles.module.css';
 import Link from 'next/link';
+import './main.css';
 import { Link as MUILink } from '@mui/material';
+
+import { DragEvent } from 'react';
 
 interface IProps {
     onCreateFolder: () => void;
@@ -17,10 +21,95 @@ export const Sidebar = ({
     onUploadFile,
 }: PropsWithChildren<IProps>) => {
     const filesInput = useRef<HTMLInputElement>(null);
+    const dropzoneRef = useRef<HTMLDivElement>(null);
+
+    const [filesLoaded, setFilesLoaded] = useState<File[]>([]);
+
+    function addFile(file: File) {
+        setFilesLoaded((prev) => {
+            const newState = [...prev, file];
+            console.log(newState);
+            return newState;
+        });
+    }
+
+    useEffect(() => {
+        if (dropzoneRef.current) {
+            dropzoneRef.current.addEventListener('dragenter', (e) => {
+                e.preventDefault();
+
+                if (!dropzoneRef.current?.classList.contains(stylesDropzone.dropzone__dragover))
+                    dropzoneRef.current?.classList.toggle(stylesDropzone.dropzone__dragover);
+            });
+
+            dropzoneRef.current.addEventListener('dragleave', (e) => {
+                e.preventDefault();
+
+                if (dropzoneRef.current?.classList.contains(stylesDropzone.dropzone__dragover))
+                    dropzoneRef.current?.classList.toggle(stylesDropzone.dropzone__dragover);
+            });
+
+            dropzoneRef.current.addEventListener('dragover', (e) => {
+                /** Need to preventDefault to let `drop` event fire */
+                e.preventDefault();
+            });
+        }
+
+        // TODO: clear
+    });
 
     return (
         <Toolbar className={styles.drawer}>
             <Stack direction="column" spacing={2}>
+                <div
+                    className={stylesDropzone.dropzone}
+                    id="dropzone"
+                    // onDragOverCapture={(e) => {
+                    //     console.log(e)
+                    // }}
+                    onClick={() => {
+                        filesInput.current?.click();
+                    }}
+                    ref={dropzoneRef}
+                    onDrop={(e: DragEvent<HTMLElement>) => {
+                        e.preventDefault();
+
+                        console.log(e.dataTransfer.items);
+                        // console.log(e.dataTransfer.files[0].name!)
+
+                        if (e.dataTransfer.items) {
+                            // Use DataTransferItemList interface to access the file(s)
+                            [...e.dataTransfer.items].forEach((item, i) => {
+                                // If dropped items aren't files, reject them
+                                if (item.kind === 'file') {
+                                    const file = item.getAsFile();
+                                    if (file !== null) {
+                                        console.log(file);
+                                        console.log(
+                                            `… file[${i}].name = ${file!.name}`
+                                        );
+
+                                        addFile(file);
+                                    }
+                                }
+                            });
+                        } else {
+                            // Use DataTransfer interface to access the file(s)
+                            [...e.dataTransfer.files].forEach((file, i) => {
+                                console.log(`… file[${i}].name = ${file.name}`);
+                            });
+                        }
+                    }}
+                    onDragOver={(e: DragEvent<HTMLElement>) => {
+                        // console.log('drag over:', e);
+                    }}
+                >
+                    {
+                        filesLoaded.map((f, index) => {
+                            return <div className={stylesDropzone.dropzone_item} key={index}>{f.name}</div>;
+                        })
+                    }
+                </div>
                 <Button
                     variant="contained"
                     color="secondary"
@@ -58,7 +147,9 @@ export const Sidebar = ({
 
                             for (const file of filesInput.current!.files!) {
                                 data.append('files', file, file.name);
+                                addFile(file);
                             }
+
                             onUploadFile(data);
                         }}
                     ></input>
