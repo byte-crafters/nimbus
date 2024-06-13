24347ec8c6ed2168d9b3001d5fad4162a152144e
Maksim Beliavsky <maksimbell503@gmail.com>
2024-06-13 19:43:11 +0300

    s5-9/fix: add header menu logout

--- frontend/src/shared/Header/Header.tsx
+++ frontend/src/shared/Header/Header.tsx
@@ -1,38 +1,21 @@
 'use client';
-import { AccountCircle } from '@mui/icons-material';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import SearchIcon from '@mui/icons-material/Search';
-import {
-    AppBar,
-    Badge,
-    Box,
-    IconButton,
-    Input,
-    Toolbar,
-    Typography,
-} from '@mui/material';
+import { AppBar, Box, Input, Toolbar, Typography } from '@mui/material';
+import clsx from 'clsx';
 import { PropsWithChildren } from 'react';
 import styles from './Header.module.scss';
-import { fetcher } from '@/libs/request';
-import { useRouter } from 'next/navigation';
+import { Navigation } from './components';
 
 export const Header = ({}: PropsWithChildren) => {
-    const router = useRouter();
-
     return (
         <Box>
-            <AppBar
-                position="static"
-                // color="secondary"
-                className={styles.bar}
-            >
+            <AppBar position="static" className={styles.bar}>
                 <Toolbar className={styles.container}>
                     <Typography
                         variant="h5"
                         noWrap
                         component="div"
-                        className={styles.name}
+                        className={clsx(styles.name)}
                     >
                         Nimbus
                     </Typography>
@@ -45,41 +28,7 @@ export const Header = ({}: PropsWithChildren) => {
                             className={styles.input}
                         />
                     </div>
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="show 4 new mails"
-                            color="inherit"
-                        >
-                            <Badge badgeContent={2} color="error">
-                                <MailIcon />
-                            </Badge>
-                        </IconButton>
-                        <IconButton
-                            size="large"
-                            aria-label="show 17 new notifications"
-                            color="inherit"
-                        >
-                            <Badge badgeContent={1} color="error">
-                                <NotificationsIcon />
-                            </Badge>
-                        </IconButton>
-                        <IconButton
-                            onClick={async () => {
-                                await fetcher.signout();
-                                router.push('/login');
-                            }}
-                            size="large"
-                            edge="end"
-                            aria-label="account of current user"
-                            // aria-controls={menuId}
-                            aria-haspopup="true"
-                            // onClick={handleProfileMenuOpen}
-                            color="inherit"
-                        >
-                            <AccountCircle />
-                        </IconButton>
-                    </Box>
+                    <Navigation />
                 </Toolbar>
             </AppBar>
         </Box>
